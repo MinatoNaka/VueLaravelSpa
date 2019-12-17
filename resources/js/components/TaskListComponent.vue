a298f652c321cfa4dc9a647082311e9e01b8503f
aq_minato.naka <minato.naka@asia-quest.jp>
2019-12-17 17:17:45 +0900

    タスク一覧ページ削除API繋ぎ込み

--- resources/js/components/TaskListComponent.vue
+++ resources/js/components/TaskListComponent.vue
@@ -29,7 +29,7 @@
                     </router-link>
                 </td>
                 <td>
-                    <button class="btn btn-danger">Delete</button>
+                    <button class="btn btn-danger" v-on:click="deleteTask(task.id)">Delete</button>
                 </td>
             </tr>
             </tbody>
@@ -49,6 +49,12 @@
                 axios.get('/api/tasks')
                     .then((res) => {
                         this.tasks = res.data;
+                    });
+            },
+            deleteTask(id) {
+                axios.delete('/api/tasks/' + id)
+                    .then((res) => {
+                        this.getTasks();
                     });
             }
         },
