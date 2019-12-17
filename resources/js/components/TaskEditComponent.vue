b80901007baeefb078f1b64c5c725f77bb3e1331
aq_minato.naka <minato.naka@asia-quest.jp>
2019-12-17 17:09:22 +0900

    タスク編集ページAPI繋ぎ込み

--- resources/js/components/TaskEditComponent.vue
+++ resources/js/components/TaskEditComponent.vue
@@ -2,22 +2,22 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-sm-6">
-                <form>
+                <form v-on:submit.prevent="submit">
                     <div class="form-group row">
                         <label for="id" class="col-sm-3 col-form-label">ID</label>
-                        <input type="text" class="col-sm-9 form-control-plaintext" readonly id="id" v-bind:value="taskId">
+                        <input type="text" class="col-sm-9 form-control-plaintext" readonly id="id" v-model="task.id">
                     </div>
                     <div class="form-group row">
                         <label for="title" class="col-sm-3 col-form-label">Title</label>
-                        <input type="text" class="col-sm-9 form-control" id="title">
+                        <input type="text" class="col-sm-9 form-control" id="title" v-model="task.title">
                     </div>
                     <div class="form-group row">
                         <label for="content" class="col-sm-3 col-form-label">Content</label>
-                        <input type="text" class="col-sm-9 form-control" id="content">
+                        <input type="text" class="col-sm-9 form-control" id="content" v-model="task.content">
                     </div>
                     <div class="form-group row">
                         <label for="person-in-charge" class="col-sm-3 col-form-label">Person In Charge</label>
-                        <input type="text" class="col-sm-9 form-control" id="person-in-charge">
+                        <input type="text" class="col-sm-9 form-control" id="person-in-charge" v-model="task.person_in_charge">
                     </div>
                     <button type="submit" class="btn btn-primary">Submit</button>
                 </form>
@@ -30,6 +30,28 @@
     export default {
         props: {
             taskId: String
+        },
+        data: function () {
+            return {
+                task: {}
+            }
+        },
+        methods: {
+            getTask() {
+                axios.get('/api/tasks/' + this.taskId)
+                    .then((res) => {
+                        this.task = res.data;
+                    });
+            },
+            submit() {
+                axios.put('/api/tasks/' + this.taskId, this.task)
+                    .then((res) => {
+                        this.$router.push({name: 'task.list'})
+                    });
+            }
+        },
+        mounted() {
+            this.getTask();
         }
     }
 </script>
