e9f9d5318326780a95da893f671c05856f628aab
aq_minato.naka <minato.naka@asia-quest.jp>
2019-12-17 16:11:17 +0900

    タスク更新API実装

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -21,4 +21,11 @@ class TaskController extends Controller
     {
         return $task;
     }
+
+    public function update(Request $request, Task $task)
+    {
+        $task->update($request->all());
+
+        return $task;
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -16,4 +16,5 @@ use Illuminate\Http\Request;
 Route::get('/tasks', 'TaskController@index');
 Route::post('/tasks', 'TaskController@store');
 Route::get('/tasks/{task}', 'TaskController@show');
+Route::put('/tasks/{task}', 'TaskController@update');
 
