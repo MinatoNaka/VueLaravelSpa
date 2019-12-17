9c97486a08bb6b42be6b9cfd7c1de5adc4e977e9
aq_minato.naka <minato.naka@asia-quest.jp>
2019-12-17 16:18:33 +0900

    タスク削除API実装

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -28,4 +28,11 @@ class TaskController extends Controller
 
         return $task;
     }
+
+    public function destroy(Task $task)
+    {
+        $task->delete();
+
+        return $task;
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,5 @@ Route::get('/tasks', 'TaskController@index');
 Route::post('/tasks', 'TaskController@store');
 Route::get('/tasks/{task}', 'TaskController@show');
 Route::put('/tasks/{task}', 'TaskController@update');
+Route::delete('/tasks/{task}', 'TaskController@destroy');
 
