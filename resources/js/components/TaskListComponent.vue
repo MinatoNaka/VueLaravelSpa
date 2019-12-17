11885ce8f9477060505859a7b249b6ec5d5a1f89
aq_minato.naka <minato.naka@asia-quest.jp>
2019-12-17 16:40:04 +0900

    タスク一覧ページAPI繋ぎ込み

--- resources/js/components/TaskListComponent.vue
+++ resources/js/components/TaskListComponent.vue
@@ -13,56 +13,18 @@
             </tr>
             </thead>
             <tbody>
-            <tr>
-                <th scope="row">1</th>
-                <td>Title1</td>
-                <td>Content1</td>
-                <td>Ichiro</td>
+            <tr v-for="task in tasks">
+                <th scope="row">{{ task.id }}</th>
+                <td>{{ task.title }}</td>
+                <td>{{ task.content }}</td>
+                <td>{{ task.person_in_charge }}</td>
                 <td>
-                    <router-link v-bind:to="{name: 'task.show', params: {taskId: 1}}">
+                    <router-link v-bind:to="{name: 'task.show', params: {taskId: task.id }}">
                         <button class="btn btn-primary">Show</button>
                     </router-link>
                 </td>
                 <td>
-                    <router-link v-bind:to="{name: 'task.edit', params: {taskId: 1}}">
-                        <button class="btn btn-success">Edit</button>
-                    </router-link>
-                </td>
-                <td>
-                    <button class="btn btn-danger">Delete</button>
-                </td>
-            </tr>
-            <tr>
-                <th scope="row">2</th>
-                <td>Title2</td>
-                <td>Content2</td>
-                <td>Jiro</td>
-                <td>
-                    <router-link v-bind:to="{name: 'task.show', params: {taskId: 2}}">
-                        <button class="btn btn-primary">Show</button>
-                    </router-link>
-                </td>
-                <td>
-                    <router-link v-bind:to="{name: 'task.edit', params: {taskId: 2}}">
-                        <button class="btn btn-success">Edit</button>
-                    </router-link>
-                </td>
-                <td>
-                    <button class="btn btn-danger">Delete</button>
-                </td>
-            </tr>
-            <tr>
-                <th scope="row">3</th>
-                <td>Title3</td>
-                <td>Content3</td>
-                <td>Saburo</td>
-                <td>
-                    <router-link v-bind:to="{name: 'task.show', params: {taskId: 3}}">
-                        <button class="btn btn-primary">Show</button>
-                    </router-link>
-                </td>
-                <td>
-                    <router-link v-bind:to="{name: 'task.edit', params: {taskId: 3}}">
+                    <router-link v-bind:to="{name: 'task.edit', params: {taskId: task.id }}">
                         <button class="btn btn-success">Edit</button>
                     </router-link>
                 </td>
@@ -76,5 +38,22 @@
 </template>
 
 <script>
-    export default {}
+    export default {
+        data: function () {
+            return {
+                tasks: []
+            }
+        },
+        methods: {
+            getTasks() {
+                axios.get('/api/tasks')
+                    .then((res) => {
+                        this.tasks = res.data;
+                    });
+            }
+        },
+        mounted() {
+            this.getTasks();
+        }
+    }
 </script>
