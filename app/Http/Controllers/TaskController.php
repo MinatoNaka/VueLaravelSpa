4cf599a4cea3863a7b1e7fb7efe80eeb31d88c88
aq_minato.naka <minato.naka@asia-quest.jp>
2019-12-17 15:47:56 +0900

    タスク詳細取得API実装

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -10,4 +10,9 @@ class TaskController extends Controller
     {
         return Task::all();
     }
+
+    public function show(Task $task)
+    {
+        return $task;
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -14,4 +14,5 @@ use Illuminate\Http\Request;
 */
 
 Route::get('/tasks', 'TaskController@index');
+Route::get('/tasks/{task}', 'TaskController@show');
 
