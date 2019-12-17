7c9b62debdeda68d380f5c06491db3922ecf1355
aq_minato.naka <minato.naka@asia-quest.jp>
2019-12-17 16:50:02 +0900

    タスク詳細ページAPI繋ぎ込み

--- resources/js/components/TaskShowComponent.vue
+++ resources/js/components/TaskShowComponent.vue
@@ -6,22 +6,22 @@
                     <div class="form-group row border-bottom">
                         <label for="id" class="col-sm-3 col-form-label">ID</label>
                         <input type="text" class="col-sm-9 form-control-plaintext" readonly id="id"
-                               v-bind:value="taskId">
+                               v-model="task.id">
                     </div>
                     <div class="form-group row border-bottom">
                         <label for="title" class="col-sm-3 col-form-label">Title</label>
                         <input type="text" class="col-sm-9 form-control-plaintext" readonly id="title"
-                               value="title title">
+                               v-model="task.title">
                     </div>
                     <div class="form-group row border-bottom">
                         <label for="content" class="col-sm-3 col-form-label">Content</label>
                         <input type="text" class="col-sm-9 form-control-plaintext" readonly id="content"
-                               value="content content">
+                               v-model="task.content">
                     </div>
                     <div class="form-group row border-bottom">
                         <label for="person-in-charge" class="col-sm-3 col-form-label">Person In Charge</label>
                         <input type="text" class="col-sm-9 form-control-plaintext" readonly id="person-in-charge"
-                               value="Ichiro">
+                               v-model="task.person_in_charge">
                     </div>
                 </form>
             </div>
@@ -33,6 +33,22 @@
     export default {
         props: {
             taskId: String
+        },
+        data: function () {
+            return {
+                task: {}
+            }
+        },
+        methods: {
+            getTask() {
+                axios.get('/api/tasks/' + this.taskId)
+                    .then((res) => {
+                        this.task = res.data;
+                    });
+            }
+        },
+        mounted() {
+            this.getTask();
         }
     }
 </script>
